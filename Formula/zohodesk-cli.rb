6898bfdbf8feeeaae645b02633c928ccc43a939d
Jose Rodriguez Ruibal <jrodriguezruibal@hotmail.com>
2026-03-18 17:09:09 +0100

    Update zohodesk-cli to v0.2.0

--- Formula/zohodesk-cli.rb
+++ Formula/zohodesk-cli.rb
@@ -1,28 +1,28 @@
 class ZohodeskCli < Formula
   desc 'CLI for Zoho Desk API'
   homepage 'https://github.com/jrodriguezruibal/zohodesk-cli'
-  version '0.1.2'
+  version '0.2.0'
   license 'MIT'
 
   on_macos do
     on_intel do
       url "https://github.com/jrodriguezruibal/zohodesk-cli/releases/download/v#{version}/zohodesk-cli_#{version}_darwin_amd64.tar.gz"
-      sha256 'f80fbd4d6808595cdf181d7b1e625f8858fcaf996a8c4f5bf7a723cc9c44c65e'
+      sha256 'a11d8080edc5812185ac857b02dea0c54edc4aef18648397a997fd89757af5ed'
     end
     on_arm do
       url "https://github.com/jrodriguezruibal/zohodesk-cli/releases/download/v#{version}/zohodesk-cli_#{version}_darwin_arm64.tar.gz"
-      sha256 '61ffa3caef11c83e9ce94e29a24b384b9763d74d5092853e0c92afd3357b3b9d'
+      sha256 '69837ff53227d98523894effb1dec5e12e3edc83b32c503311fa352d39e45e57'
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/jrodriguezruibal/zohodesk-cli/releases/download/v#{version}/zohodesk-cli_#{version}_linux_amd64.tar.gz"
-      sha256 'f0d2f091049879a3154a7cd240bda5131eca4ba75e7afbd1d0e7baf43228e888'
+      sha256 '4d33cfacdabdca5e6b166da95b0911cce6fe70a6c40d2af45579b42b94c52ecf'
     end
     on_arm do
       url "https://github.com/jrodriguezruibal/zohodesk-cli/releases/download/v#{version}/zohodesk-cli_#{version}_linux_arm64.tar.gz"
-      sha256 'b2b4b006b761d12cd47e4acbc8f18e1498f785f61eb455d728dc4e9ca17f6937'
+      sha256 '19f2f3bb22db4f910e4ecfadf2b6a52a7b8033b21aff3a1cd7587ef9dc0627ce'
     end
   end
 
